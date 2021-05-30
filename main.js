f9c5399fda1cf90790bff0525e2915dfde1a5e33
julian-pulpo <julian.schrodinger@gmail.com>
2021-05-29 21:35:17 -0500

    añado la linea dibujada para ensayar

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 var d = document.getElementsByClassName("lienzo")[0].getContext("2d");
+
+
+d.beginPath();
+d.strokeStyle = "red";
+d.moveTo(100,100);
+d.lineTo(300,300);
+d.stroke();
+d.closePath();
