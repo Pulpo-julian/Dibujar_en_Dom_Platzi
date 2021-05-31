91f456f98e8da772788fe5ed2b43512595b6a3d5
julian-pulpo <julian.schrodinger@gmail.com>
2021-05-30 23:16:26 -0500

    añado cambios al js

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ var d = document.getElementsByClassName("lienzo")[0].getContext("2d");
 
 
 d.beginPath();
-d.strokeStyle = "red";
-d.moveTo(100,100);
-d.lineTo(300,300);
+d.strokeStyle = "green";
+d.moveTo(150,150);
+d.lineTo(100,100);
 d.stroke();
 d.closePath();
