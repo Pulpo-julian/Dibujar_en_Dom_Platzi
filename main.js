ae586869372a664f1ab3d433d191ebeb953c7122
julian-pulpo <julian.schrodinger@gmail.com>
2021-05-30 23:21:26 -0500

    añado footer y cambios al js

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ var d = document.getElementsByClassName("lienzo")[0].getContext("2d");
 
 
 d.beginPath();
-d.strokeStyle = "green";
-d.moveTo(150,150);
-d.lineTo(100,100);
+d.strokeStyle = "black";
+d.moveTo(89,140);
+d.lineTo(160,160);
 d.stroke();
 d.closePath();
