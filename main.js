741591889b8ee98421cca0b267ae102f6b2e7b81
julian-pulpo <julian.schrodinger@gmail.com>
2021-06-02 16:15:16 -0500

    dejo solamente el lienzo y las funciones de dibjuar en js

--- main.js
+++ main.js
@@ -1,30 +1,4 @@
-iBoton.addEventListener("click", () => {
-    dibujarPorClick();
-});
-
 var d = document.getElementsByClassName("lienzo")[0].getContext("2d");
-
-
-
-
-var lineas = parseInt(iTexto.value);
-var yi, xf;
-var colorcito = "red";
-var recorrido = d.canvas.width / lineas;
-
-
-for (l = 0; l < lineas; l++) {
-    yi = recorrido * l;
-    xf = recorrido * (l + 1);
-    dibujarLinea(colorcito, 0, yi, xf, 300);
-}
-
-dibujarLinea(colorcito, 0, 0, 0, 300)
-dibujarLinea(colorcito, 300, 0, 0, 0)
-dibujarLinea(colorcito, 300, 0, 300, 300)
-dibujarLinea(colorcito, 0, 300, 300, 300)
-
-
 
 
 function dibujarLinea (color, xi, yi, xf, yf) {
@@ -35,3 +9,8 @@ function dibujarLinea (color, xi, yi, xf, yf) {
     d.stroke();
     d.closePath();
 }
+
+dibujarLinea(colorcito, 0, 0, 0, 300)
+dibujarLinea(colorcito, 300, 0, 0, 0)
+dibujarLinea(colorcito, 300, 0, 300, 300)
+dibujarLinea(colorcito, 0, 300, 300, 300)
