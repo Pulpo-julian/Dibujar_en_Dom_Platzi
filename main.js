2aa9b626393cffa13572a1255bca40116590b5c5
julian-pulpo <julian.schrodinger@gmail.com>
2021-06-02 18:50:26 -0500

    acabo script.. faltan mejoras

--- main.js
+++ main.js
@@ -2,10 +2,14 @@ document.addEventListener("keyup", dibujarTeclas);
 
 var d = document.getElementsByClassName("lienzo")[0].getContext("2d");
 
+dibujarLinea("#AFA", 149, 149, 151, 151);
+
+
 
 function dibujarLinea (color, xi, yi, xf, yf) {
     d.beginPath();
     d.strokeStyle = color;
+    d.lineWidth = 3;
     d.moveTo(xi,yi);
     d.lineTo(xf,yf);
     d.stroke();
@@ -19,12 +23,37 @@ var teclas = {
     RIGTH: 39
 };
 
+var x = 150;
+var y = 150;
+movimiento = 10;
+
 function dibujarTeclas (evento) {
-    console.log(evento.keyCode);
+    if (evento.keyCode == teclas.UP) {
+
+        console.log("es la de arriba");
+        dibujarLinea("#AFA", x, y, x, y - movimiento);
+        y = y - movimiento;
+
+    } else if (evento.keyCode == teclas.DOWN) {
+
+        console.log("es la de abajo");
+        dibujarLinea("#AFA", x, y, x, y + movimiento);
+        y = y + movimiento;
+
+    } else if (evento.keyCode == teclas.LEFT) {
+
+        console.log("es la de la izquierda");
+        dibujarLinea("#AFA", x, y, x - movimiento, y);
+        x = x - movimiento;
+    } else if (evento.keyCode == teclas.RIGTH) {
+
+        console.log("es la de la derecha");
+        dibujarLinea("#AFA", x, y, x + movimiento, y);
+        x = x + movimiento;
+    }
 }
 
 
-// dibujarLinea(colorcito, 0, 0, 0, 300)
 // dibujarLinea(colorcito, 300, 0, 0, 0)
 // dibujarLinea(colorcito, 300, 0, 300, 300)
 // dibujarLinea(colorcito, 0, 300, 300, 300)
