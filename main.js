40cb13f7f14789ea0e2b8d8ae2e60739263660d2
julian-pulpo <julian.schrodinger@gmail.com>
2021-06-01 23:34:27 -0500

    añado en js todo el codigo

--- main.js
+++ main.js
@@ -1,26 +1,34 @@
 var iTexto = document.getElementsByClassName("entrada")[0];
 var iBoton = document.getElementsByClassName("entrada")[1];
 
-
+iBoton.addEventListener("click", () => {
+    dibujarPorClick();
+});
 
 var d = document.getElementsByClassName("lienzo")[0].getContext("2d");
-var lineas = 30;
-var l = 0;
-var yi, xf;
-var colorcito = "#faf";
 
 
-for (l = 0; l<lineas; l++) {
-    yi = 10 * l;
-    xf = 10 * (l +1);
-    dibujarLinea(colorcito, 0, yi, xf, 300);
-    l = l + 1;
+
+function dibujarPorClick() {
+    var lineas = parseInt(iTexto.value);
+    var yi, xf;
+    var colorcito = "red";
+    var recorrido = d.canvas.width / lineas;
+    
+
+    for (l = 0; l < lineas; l++) {
+        yi = recorrido * l;
+        xf = recorrido * (l + 1);
+        dibujarLinea(colorcito, 0, yi, xf, 300);
+    }
+
+    dibujarLinea(colorcito, 0, 0, 0, 300)
+    dibujarLinea(colorcito, 300, 0, 0, 0)
+    dibujarLinea(colorcito, 300, 0, 300, 300)
+    dibujarLinea(colorcito, 0, 300, 300, 300)
 }
 
-dibujarLinea(colorcito, 0, 0, 0, 300)
-dibujarLinea(colorcito, 300, 0, 0, 0)
-dibujarLinea(colorcito, 300, 0, 300, 300)
-dibujarLinea(colorcito, 0, 300, 300, 300)
+
 
 function dibujarLinea (color, xi, yi, xf, yf) {
     d.beginPath();
